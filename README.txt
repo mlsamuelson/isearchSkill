7da1e1d28b29b6446cad47fe3a4fda58a1c28092
mlsamuelson <mlsamuel@asu.edu>
2017-08-17 13:15:45 -0700

    Update README, implement speak tags consistently.

--- README.txt
+++ README.txt
@@ -5,9 +5,16 @@ Built using the Flask-ask library.
 
 Project is in Proof of Concept stage.
 
+Directory_VUI_v_2_0 : https://www.lucidchart.com/invitations/accept/64facdbf-dba7-4c2c-9292-14e6d4d23f8b
+
 ROADMAP
 - Uncomment code for displaying photos in cards when CORS has been enabled on the Photos service. Laura C. TODO
 - fuzzy up the Solr query for better matching on names
 - add VIP's last names to sample LAST_NAME slot values
-- can we use Flask caching for results within Flask-ask? http://flask.pocoo.org/docs/0.12/patterns/caching/
+- better pronunciation for ASU-specific words and abbreviations via SSML <phoneme> and <say-as> tags.
+- X can we use Flask caching for results within Flask-ask? http://flask.pocoo.org/docs/0.12/patterns/caching/
+  - caching already in Flask-ask library: cache.py
 - separate code and response formatting better. Perhaps levarage Flask-ask Jinja templating.
+
+NOTES
+- Dialog Delegation support notes: https://github.com/johnwheeler/flask-ask/pull/165
--- isearch.py
+++ isearch.py
@@ -7,6 +7,9 @@ import requests
 #import unidecode
 import logging
 
+# DEBUGGING
+# import pdb
+
 # Params
 SOLR = 'https://asudir-solr.asu.edu/asudir/'
 # Example people query:
@@ -64,11 +67,11 @@ def get_people_results_output(record):
 
     # out = record['firstName'] + ' ' + record['lastName'] + ', '
     out = record.get('displayName', '')
-    out += ',,Title: ' + record.get('primaryTitle', '')
-    out += ',,Department: ' + record.get('primaryiSearchDepartmentAffiliation', '')
-    out += ',,Email address: ' + record.get('emailAddress', '')
-    out += ',,Phone: <say-as interpret-as="telephone">' + record.get('phone', '') + '</say-as>'  # say-as telephone
-    out += ',,Mail code: <say-as interpret-as="digits">' + record.get('primaryMailcode', '') + '</say-as>'  # say-as digits
+    out += ',,,Title:,, ' + record.get('primaryTitle') if record.get('primaryTitle') else ''
+    out += ',,,Department:,, ' + record.get('primaryiSearchDepartmentAffiliation') if record.get('primaryiSearchDepartmentAffiliation') else ''
+    out += ',,,Email address:,, ' + record.get('emailAddress') if record.get('emailAddress') else ''
+    out += ',,,Phone:,, <say-as interpret-as="telephone">' + record.get('phone') + '</say-as>' if record.get('phone') else ''
+    out += ',,,Mail code:,, <say-as interpret-as="digits">' + record.get('primaryMailcode') + '</say-as>' if record.get('primaryMailcode') else ''
     return out
 
 def get_people_results_card(record):
@@ -89,26 +92,25 @@ def get_people_results_card_photo_url(record):
 
 @ask.launch  # User starts skill without any intent.
 def launch():
-    welcome_message = '<speak>Welcome to the <say-as interpret-as="spell-out">ASU</say-as> <say-as interpret-as="spell-out">i</say-as>Search Directory. Search people by saying something like "ask directory to find Michael Crow."</speak>'
-    return statement(welcome_message)
+    welcome_message = 'Welcome to the <say-as interpret-as="spell-out">ASU</say-as> <say-as interpret-as="spell-out">i</say-as>Search Directory. Search people by saying something like "ask directory to find Michael Crow."'
+    return statement("<speak>{}</speak>".format(welcome_message))
 
 @ask.intent('iSearchIntentPeopleFirst')
 def get_first_isearch_people_results(firstName, lastName):
-    reprompt_text = '<speak>To search the <say-as interpret-as="spell-out">ASU</say-as> <say-as interpret-as="spell-out">i<say-as interpret-as="spell-out">Search Directory for a person, try asking something like "find Michael Crow"</speak>'
+    reprompt_text = 'To search the <say-as interpret-as="spell-out">ASU</say-as> <say-as interpret-as="spell-out">i<say-as interpret-as="spell-out">Search Directory for a person, try asking something like "find Michael Crow"'
     if firstName or lastName:
         results = get_people_results(firstName, lastName)
     else:
-        return statement(reprompt_text)
+        return statement("<speak>{}</speak>".format(reprompt_text))
 
     no_results_response = "I didn't find any results for {} {}.".format(firstName, lastName)
     if results == None:
-        return statement(no_results_response)
+        return statement("<speak>{}</speak>".format(no_results_response))
     if len(results) < 1:
-        return statement(no_results_response)
+        return statement("<speak>{}</speak>".format(no_results_response))
 
-    # TODO separate formatting better
-    speech_output = "For {} {} in people".format(firstName, lastName)  # Start speech string.
-    card_title = "Results for {} {} in people".format(firstName, lastName)
+    speech_output = "For search {} {}".format(firstName, lastName)  # Start speech string.
+    card_title = "Results for {} {}".format(firstName, lastName)
     card_output = ""
     card_photo = ""
     range_value = PAGINATION_SIZE if len(results) >= PAGINATION_SIZE else len(results)
@@ -126,20 +128,19 @@ def get_first_isearch_people_results(firstName, lastName):
     # CORS enabled photo for testing
     #card_photo='https://i.imgur.com/hYQzVO3.jpg'
 
-    speech_output = "<speak>{}</speak>".format(speech_output)
+    #speech_output = "<speak>{}</speak>".format(speech_output)
     if len(card_photo) > 0:
         # TODO issue with CORS for images?
         # See "Hosting the Images" on https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/providing-home-cards-for-the-amazon-alexa-app
 
-        # TODO better guarding against missing data bits/errors.
-        return question(speech_output) \
+        return question("<speak>{}</speak>".format(speech_output)) \
             .reprompt(reprompt_text) \
             .standard_card(title=card_title,
                            text=card_output,
                            small_image_url=card_photo + '?size=small',
                            large_image_url=card_photo + '?size=large')
     else:
-        return question(speech_output) \
+        return question("<speak>{}</speak>".format(speech_output)) \
             .reprompt(reprompt_text) \
             .simple_card(title=card_title,
                          content=card_output)
@@ -156,9 +157,9 @@ def get_next_isearch_people_results():
     card_output = ""
     card_photo = ""
     i = 0
-    if(index >= len(results)):
+    if index >= len(results):
         speech_output += " End of results."
-        return statement(speech_output)
+        return statement("<speak>{}</speak>".format(speech_output))
 
     else:
         while i < PAGINATION_SIZE and index < len(results):
@@ -175,16 +176,16 @@ def get_next_isearch_people_results():
     session.attributes[SESSION_SLOT_FIRSTNAME] = firstName
     session.attributes[SESSION_SLOT_LASTNAME] = lastName
 
-    speech_output = '<speak>{}</speak>'.format(speech_output)
+    #speech_output = '<speak>{}</speak>'.format(speech_output)
     if len(card_photo) > 0:
-        return question(speech_output) \
+        return question('<speak>{}</speak>'.format(speech_output)) \
             .reprompt(reprompt_text) \
             .standard_card(title=card_title,
                            text=card_output,
                            small_image_url=card_photo + '?size=small',
                            large_image_url=card_photo + '?size=large')
     else:
-        return question(speech_output) \
+        return question('<speak>{}</speak>'.format(speech_output)) \
             .reprompt(reprompt_text) \
             .simple_card(title=card_title,
                          content=card_output)
